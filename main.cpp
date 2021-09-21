e656480a7f328d2f5575cfb674b474dd48d3f940
zavier-wong <52955418+zavier-wong@users.noreply.github.com>
2021-09-21 23:47:47 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 
 int main(int argc,char *argv[])
 {
-	char *str=new char[100];
-	strcpy(str,"Hello,World");
+    char *str=new char[100];
+    strcpy(str,"Hello,World");
     printf("%s \n",str);
     return 0;
 }
